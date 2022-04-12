0764a6d906d922d0524b101bc0497dbe2ca00c83
ilhammhrrm <90167055+ilhammhrrm@users.noreply.github.com>
2022-04-12 14:49:15 +0700

    Update Fibonacci rekursif.cpp

--- Fibonacci rekursif.cpp
+++ Fibonacci rekursif.cpp
@@ -1,51 +1,33 @@
 #include <iostream>
-
 using namespace std;
 
-// Membuat fungsi bernama fibonacci yang bertipe integer dan menerima masukkan sebuah integer yang diberi nama variable n.
+int main()
+{
+    int n, f1 = 0, f2 = 1, berikutnya = 0;
 
-int fibonacci(int n) {
+    cout << "Masukan Batas Deret Bilangan Fibonacci :  ";
+    cin >> n;
+	  cout<<endl;
+    cout << "Deret Fibonacci: ";
 
-// Melakukan cek kondisi jika nilai dari variable n adalah 0 atau 1 maka akan mengembalikan nilai n.
-
-  if (n == 0 || n ==1){
-
-    return n;
-
-//  Jika variable n bernilai selain 0 dan 1 maka akan melakukan operasi rekursif dimana hasil operasi fibonacci n-1 ditambah dengan hasil operasi fibonacci n-2
-
-  } else {
-
-    return (fibonacci(n-1) + fibonacci(n-2));
-
-  }
-
-}
-
-int main() {
-
-// mendeklarasikan variable n, i, dan j dimana variable n digunakan untuk batas jumlah bilangan fibonacci yang akan ditampilkan
-
-  int n, i, j = 0;
-
-  cout << "Masukkan batas jumlah bilangan fibonacci: ";
-
-  // menginputkan nilai kedalam variable n.
-
-  cin >> n;
-
-  cout << "Hasil bilangan fibonacci: \n";
-
-  for (i = 1; i <= n; i++){
-
-    cout << "fibonacci ke-"<< i <<": " << fibonacci(j) << endl;
-
-    j++;
-
-// melakukan perulangan sebanyak nilai n dan menginputkan nilai j kedalam fungsi fibonacci serta menampilkan hasil operasi fibonacci
-
-  }
-
-  return 0;
-
+    for (int i = 1; i <= n; ++i)
+    {
+        // Mencetak dua deret bilangan fibonacci pertama.
+        if(i == 1)
+        {
+            cout << " " << f1<<" ";
+            continue;
+        }
+        if(i == 2)
+        {
+            cout << f2 << " ";
+            continue;
+        }
+        berikutnya = f1 + f2;
+        f1 = f2;
+        f2 = berikutnya;
+         // Mencetak deret bilangan fibonacci berikutnya.
+        cout << berikutnya << " ";
+    }
+    return 0;
 }
