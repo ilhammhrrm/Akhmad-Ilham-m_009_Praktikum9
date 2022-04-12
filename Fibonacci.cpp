2d04f0a1ac1f709ba09442ba8bfed110c6a82785
ilhammhrrm <90167055+ilhammhrrm@users.noreply.github.com>
2022-04-12 15:07:31 +0700

    Update Fibonacci.cpp

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -1,32 +1,23 @@
 #include <iostream>
+
 using namespace std;
 
-int main()
-{
-    int n, f1 = 0, f2 = 1, berikutnya = 0;
-
-    cout << "Masukan Batas Deret Bilangan Fibonacci :  ";
-    cin >> n;
-    cout<<endl;
-    cout << "Deret Fibonacci: ";
-    for (int i = 1; i <= n; ++i)
-    {
-        // Mencetak dua deret bilangan fibonacci pertama.
-        if(i == 1)
-        {
-            cout << " " << f1<<" ";
-            continue;
-        }
-        if(i == 2)
-        {
-            cout << f2 << " ";
-            continue;
-        }
-        berikutnya = f1 + f2;
-        f1 = f2;
-        f2 = berikutnya;
-         // Mencetak deret bilangan fibonacci berikutnya.
-        cout << berikutnya << " ";
+int fibonacci(int n){
+    if(n == 0 || n == 1){
+        return n;
+    } else {
+        return(fibonacci(n-1) + fibonacci(n-2));
     }
-    return 0;
+}
+int main(){
+  int n, i, j = 0;
+  cout << "batas deret : ";
+  cin >> n;
+  cout << "deret fibonacci : \n";
+  for (i = 1; i <= n; i++){
+    cout << "f_"<< i << ":";
+    cout << fibonacci(j) <<endl;
+    j++;
+  }
+  return 0;
 }
